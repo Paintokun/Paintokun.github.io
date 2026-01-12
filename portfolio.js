b3de09178ab51a3f00883f1a269c489a682029db
Paintokun <rojassanrick@gmail.com>
2026-01-11 19:15:15 -0800

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,35 +1,6 @@
-
 window.onload = () => {
   const welcome = document.querySelector('.Video_Text p');
   if (welcome) {
-    welcome.textContent = "Welcome to Sanrick Rojas' Portfolio!";
+    welcome.textContent = "Hello! Welcome to my portfolio.";
   }
 };
-let slideIndex = 0;
-const slides = document.querySelectorAll(".mySlides");
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-
-function showSlide(index) {
-  if (index >= slides.length) slideIndex = 0;
-  if (index < 0) slideIndex = slides.length - 1;
-  for (let slide of slides) {
-    slide.style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
-}
-
-function nextSlide() {
-  slideIndex++;
-  showSlide(slideIndex);
-}
-
-function prevSlide() {
-  slideIndex--;
-  showSlide(slideIndex);
-}
-
-next.addEventListener("click", nextSlide);
-prev.addEventListener("click", prevSlide);
-
-showSlide(slideIndex);
